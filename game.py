44cab12bf6cd80146cf038ca6719ba1585a016e1
KaleC <kaleb.corrigan@ascte.org>
2026-09-15 09:55:02 -0500

    Updated Game.py

--- game.py
+++ game.py
@@ -4,12 +4,16 @@ from hero import Hero
 ARENA_NAME = "THE BASEMENT"
 
 def battle(hero: Hero, enemy: Goblin):
+    round = 1
+
     while hero.is_alive() and enemy.is_alive():
+        print(f"\nROUND {round}. FIGHT")
         enemy.take_damage(hero.attack())
         if enemy.is_alive():
             hero.take_damage(enemy.attack(), enemy)
+        round += 1
     if hero.is_alive():
-        print(f"{hero.name} wins")
+        print(f"{hero.name} wins with {hero.health} health")
     else:
         print(f"{hero.name} has lost")
 
@@ -24,14 +28,15 @@ def main():
     hero = Hero("Jason", "rogue")
 
     print("")
-    print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    print(f"{goblin2.name} Enters the Arena from claires house with {goblin2.health} health.")
+    print(f"{goblin.name} enters {ARENA_NAME} with {goblin.health} health.")
+    print(f"{goblin2.name} enters {ARENA_NAME} from Claire's house with {goblin2.health} health.")
 
     print("")
     print(f"{hero.name} the {hero.hero_class} enters the arena with {hero.health} health.")
     print("")
 
     battle(hero, goblin2)
+    battle(hero, goblin)
     
 if __name__ == "__main__":
     main()
